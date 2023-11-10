b3ac4dbd990313fc028f4ecff8b92c1cc522f64c
Kiran4753 <kirankharsel@gmail.com>
2023-11-10 14:47:54 +0530

    add:  variables

---
+++ app.js
@@ -0,0 +1,12 @@
+const questions ={
+    question: '1) 2+2',
+    ansA: '1',
+    ansB: '1',
+    ansC: '1',
+    ansD: '1',
+    rightAns: 'B'
+}
+
+
+const question = document.querySelector('.questions');
+question.innerText = questions.question
