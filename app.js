cec3d63dc230ed568bacdc13f6b057702119d262
Kiran4753 <kirankharsel@gmail.com>
2023-11-11 11:50:16 +0530

    fix: next question option

--- app.js
+++ app.js
@@ -1,12 +1,68 @@
-const questions ={
-    question: '1) 2+2',
-    ansA: '1',
-    ansB: '1',
-    ansC: '1',
-    ansD: '1',
-    rightAns: 'B'
-}
+const Questions =[
+    {
+        q: "What is capital of India?",
+        a: [{ text: "Gandhinagar", isCorrect: false },
+        { text: "Surat", isCorrect: false },
+        { text: "Delhi", isCorrect: true },
+        { text: "Mumbai", isCorrect: false }
+        ]
+    },
+    {
+        q: "What is the capital of Thailand?",
+        a: [{ text: "Lampang", isCorrect: false },
+        { text: "Phuket", isCorrect: false },
+        { text: "Ayutthaya", isCorrect: false },
+        { text: "Bangkok", isCorrect: true }
+        ]
+     
+    },
+]
 
 
 const question = document.querySelector('.questions');
-question.innerText = questions.question
+const ansewrs = document.querySelector('.answers');
+const showScore = document.querySelector('.score');
+const nextBtn = document.querySelector('.next-btn')
+
+
+let currQuestion = 0
+
+let score = 0
+
+
+function loadQuestion(){
+    question.innerText = Questions[currQuestion].q;
+    // empty previous answers
+    ansewrs.innerHTML = ''
+
+    //load answer
+    Questions[currQuestion].a.forEach(element => {
+        console.log(element);
+        const ans = document.createElement('div');
+        ans.className = 'answer-option';
+        ans.textContent = element.text
+        ans.addEventListener('click', function(){
+            console.log('ok')
+            if(element.isCorrect === true){
+                score = score + 5
+                showScore.textContent = `score : ${score}`
+            }
+        }, {once:true})
+
+        ansewrs.appendChild(ans)
+    });
+}
+
+loadQuestion()
+
+nextBtn.addEventListener('click', function(){
+    if(currQuestion < Questions.length - 1){
+        currQuestion++;
+        console.log(currQuestion)
+        loadQuestion()
+        
+    }else{
+        currQuestion = 0;
+        loadQuestion()
+    }
+})
