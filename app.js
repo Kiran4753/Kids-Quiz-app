424242043808a14bd78a9a17658a2a40fc7a6337
Kiran4753 <kirankharsel@gmail.com>
2023-11-11 15:40:39 +0530

    fix: score and  style: added right answer style

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Questions =[
     {
-        q: "What is capital of India?",
+        q: "1) What is capital of India?",
         a: [{ text: "Gandhinagar", isCorrect: false },
         { text: "Surat", isCorrect: false },
         { text: "Delhi", isCorrect: true },
@@ -8,11 +8,47 @@ const Questions =[
         ]
     },
     {
-        q: "What is the capital of Thailand?",
+        q: "2) What is the capital of Thailand?",
         a: [{ text: "Lampang", isCorrect: false },
         { text: "Phuket", isCorrect: false },
         { text: "Ayutthaya", isCorrect: false },
         { text: "Bangkok", isCorrect: true }
+        ]
+     
+    },
+    {
+        q: "3) Which is the largest planet in the solar system?",
+        a: [{ text: "mars", isCorrect: false },
+        { text: "uranus", isCorrect: false },
+        { text: "jupiter", isCorrect: true },
+        { text: "saturn", isCorrect: false }
+        ]
+     
+    },
+    {
+        q: "4) How many Olympic rings are there? ",
+        a: [{ text: "5", isCorrect: true },
+        { text: "6", isCorrect: false },
+        { text: "7", isCorrect: false },
+        { text: "4", isCorrect: false }
+        ]
+     
+    },
+    {
+        q: "5) Which is the world’s largest ocean?",
+        a: [{ text: "southern oceaan", isCorrect: false },
+        { text: "atlantic ocean", isCorrect: false },
+        { text: "indian ocean", isCorrect: false },
+        { text: "pacific ocean", isCorrect: true }
+        ]
+     
+    },
+    {
+        q: "6) From which country did the Statue of Liberty come from?",
+        a: [{ text: "austria", isCorrect: false },
+        { text: "germany", isCorrect: false },
+        { text: "france", isCorrect: true },
+        { text: "britain", isCorrect: false }
         ]
      
     },
@@ -37,15 +73,18 @@ function loadQuestion(){
 
     //load answer
     Questions[currQuestion].a.forEach(element => {
-        console.log(element);
+        
         const ans = document.createElement('div');
         ans.className = 'answer-option';
         ans.textContent = element.text
         ans.addEventListener('click', function(){
-            console.log('ok')
+            
             if(element.isCorrect === true){
                 score = score + 5
-                showScore.textContent = `score : ${score}`
+                showScore.textContent = score;
+                ans.classList.add('right')
+            }else{
+                ans.classList.add('wrong')
             }
         }, {once:true})
 
@@ -58,11 +97,12 @@ loadQuestion()
 nextBtn.addEventListener('click', function(){
     if(currQuestion < Questions.length - 1){
         currQuestion++;
-        console.log(currQuestion)
         loadQuestion()
         
     }else{
         currQuestion = 0;
+        score = 0;
+        showScore.textContent = score;
         loadQuestion()
     }
 })
